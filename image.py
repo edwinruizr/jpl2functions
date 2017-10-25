a8797af6292ac94ea9fcf3db0702030025133024
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-10-25 16:27:52 -0700

    got x/y coordinate proportion

--- image.py
+++ image.py
@@ -3,6 +3,7 @@ from osgeo import ogr
 from osgeo import osr
 from osgeo import gdal_array
 from osgeo import gdalconst
+import pyproj
 import struct
 import numpy
 import sys
@@ -33,6 +34,14 @@ def getAverage(openedGDALfile):
     numpyTiffArray = numpy.array(openedGDALfile.ReadAsArray())
     tiffArray = ma.masked_array(numpyTiffArray, numpyTiffArray == noValue)
     return tiffArray.mean()
+
+def getXCoordinate(indexX):
+    return (indexX/128)-180
+
+
+def getYCoordinate(indexY):
+    return (indexY/128)-90*-1
+
 #
 #
 #FUNCTION DEFINITIONS - END
@@ -64,7 +73,7 @@ thNumpyArray  = numpy.array(thTifFile.ReadAsArray())
 
 
 # get origin and pixel size of slope tif file
-print 'LRO_NAC_Slope_15m_20N010E_2mp.tif'
+print('LRO_NAC_Slope_15m_20N010E_2mp.tif')
 geotransform = slopeTifFile.GetGeoTransform()
 if geotransform:
     print("Origin of slope tiff = ({}, {})".format(geotransform[0], geotransform[3]))
@@ -74,9 +83,10 @@ if geotransform:
 arrayOfIndexes=getMaxIndex2d(slopeTifFile)
 arrayOfMinIndexes = getMinIndex2d(slopeTifFile)
 
-print 'Max value is at index [', arrayOfIndexes[0][0], '][', arrayOfIndexes[1][0], '] = ', slopeNumpyArray[arrayOfIndexes[0][0]][arrayOfIndexes[1][0]]
-print 'Min value is at index [', arrayOfMinIndexes[0][0], '][', arrayOfMinIndexes[1][0], '] = ', slopeNumpyArray[arrayOfMinIndexes[0][0]][arrayOfMinIndexes[1][0]]
-print 'Average value is ', getAverage(slopeTifFile)
+#print ('Max value is at index [', arrayOfIndexes[0][0], '][', arrayOfIndexes[1][0], '] = ', slopeNumpyArray[arrayOfIndexes[0][0]][arrayOfIndexes[1][0]])
+print ('Max value is at index [{}][{}] = {}'.format(arrayOfIndexes[0][0],arrayOfIndexes[1][0],slopeNumpyArray[arrayOfIndexes[0][0]][arrayOfIndexes[1][0]]))
+print ('Min value is at index [{}][{}] = {}'.format(arrayOfMinIndexes[0][0], arrayOfMinIndexes[1][0],slopeNumpyArray[arrayOfMinIndexes[0][0]][arrayOfMinIndexes[1][0]]))
+print ('Average value is {}'.format(getAverage(slopeTifFile)))
 
 
 
@@ -84,7 +94,7 @@ print 'Average value is ', getAverage(slopeTifFile)
 
 
 # get origin and pixel size of fe tif file
-print 'LP_GRS_Fe_Global_2ppd.tif'
+print('LP_GRS_Fe_Global_2ppd.tif')
 transform = feTiffFile.GetGeoTransform()
 if transform:
     print("Origin of FE tiff file = ({}, {})".format(transform[0], transform[3]))
@@ -93,14 +103,14 @@ if transform:
 # calculate max, min, and average for fe tif file
 indexOfMax = getMaxIndex2d(feTiffFile)
 indexOfMin = getMinIndex2d(feTiffFile)
-print 'Max value of Fe Global tiff file is at index [', indexOfMax[0][0], '][', indexOfMax[1][0], '] = ', feNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]
-print 'Min value of Fe Global tiff file is at index [', indexOfMin[0][0], '][', indexOfMin[1][0], '] = ', feNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]
-print 'Mean value of Fe Global tiff file is', getAverage(feTiffFile)
+print("Max value of Fe Global tiff file is at index [{}][{}] = {}".format(indexOfMax[0][0], indexOfMax[1][0], feNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]))
+print('Min value of Fe Global tiff file is at index [{}][{}] = {}'.format(indexOfMin[0][0], indexOfMin[1][0], feNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]))
+print('Mean value of Fe Global tiff file is {}'.format(getAverage(feTiffFile)))
 
 
 
 # get origin and pixel size of lola dem tif file
-print 'LRO_LOLA_DEM_Global_128ppd_v04.tif'
+print('LRO_LOLA_DEM_Global_128ppd_v04.tif')
 transform = lolademFile.GetGeoTransform()   # overwrite our old variable transform
 if transform:
     print("Origin of lola dem tiff file = ({}, {})".format(transform[0], transform[3]))
@@ -109,15 +119,19 @@ if transform:
 # calculate max, min, and average for lola dem tif file
 indexOfMax = getMaxIndex2d(lolademFile)
 indexOfMin = getMinIndex2d(lolademFile)
-print 'Max value of lola dem tiff file is at index [', indexOfMax[0][0], '][', indexOfMax[1][0], '] = ', lolaNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]
-print 'Min value of lola dem tiff file is at index [', indexOfMin[0][0], '][', indexOfMin[1][0], '] = ', lolaNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]
-print 'Mean value of lola dem tiff file is', getAverage(lolademFile)
-
+print("Max value of lola dem tiff file is at index [{}][{}] = {}".format(indexOfMax[0][0], indexOfMax[1][0], lolaNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]))
+print('Min value is at index [{}][{}] = {}'.format(indexOfMin[0][0], indexOfMin[1][0], lolaNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]))
+print('Mean value of lola dem tiff file is {}'.format(getAverage(lolademFile)))
+print("Projection is {}".format(lolademFile.GetProjection()))
+x = getXCoordinate(indexOfMax[0][0])
+y = getYCoordinate(indexOfMax[1][0])
+print('max x coordinate : {}'.format(x))
+print('max y coordinate : {}'.format(y))
 
 
 
 # get origin and pixel size of H tif file
-print 'LP_GRS_H_Global_2ppd.tif'
+print('LP_GRS_H_Global_2ppd.tif')
 transform = hTifFile.GetGeoTransform()   # overwrite our old variable transform
 if transform:
     print("Origin of H tiff file = ({}, {})".format(transform[0], transform[3]))
@@ -126,15 +140,15 @@ if transform:
 # calculate max, min, and average for H tif file
 indexOfMax = getMaxIndex2d(hTifFile)
 indexOfMin = getMinIndex2d(hTifFile)
-print 'Max value of H tiff file is at index [', indexOfMax[0][0], '][', indexOfMax[1][0], '] = ', hNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]
-print 'Min value of H tiff file is at index [', indexOfMin[0][0], '][', indexOfMin[1][0], '] = ', hNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]
-print 'Mean value of H tiff file is', getAverage(hTifFile)
+print("Max value is at index [{}][{}] = {}".format(indexOfMax[0][0], indexOfMax[1][0], hNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]))
+print('Min value is at index [{}][{}] = {}'.format(indexOfMin[0][0], indexOfMin[1][0], hNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]))
+print('Mean value of H tiff file is {}'.format(getAverage(hTifFile)))
 
 
 
 
 # get origin and pixel size of K tif file
-print 'LP_GRS_K_Global_halfppd.tif'
+print('LP_GRS_K_Global_halfppd.tif')
 transform = kTifFile.GetGeoTransform()   # overwrite our old variable transform
 if transform:
     print("Origin of K tiff file = ({}, {})".format(transform[0], transform[3]))
@@ -143,13 +157,13 @@ if transform:
 # calculate max, min, and average for K tif file
 indexOfMax = getMaxIndex2d(kTifFile)
 indexOfMin = getMinIndex2d(kTifFile)
-print 'Max value is at index [', indexOfMax[0][0], '][', indexOfMax[1][0], '] = ', kNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]
-print 'Min value is at index [', indexOfMin[0][0], '][', indexOfMin[1][0], '] = ', kNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]
-print 'Mean value is', getAverage(kTifFile)
+print("Max value is at index [{}][{}] = {}".format(indexOfMax[0][0], indexOfMax[1][0], kNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]))
+print('Min value is at index [{}][{}] = {}'.format(indexOfMin[0][0], indexOfMin[1][0], kNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]))
+print('Mean value is {}'.format(getAverage(kTifFile)))
 
 
 # get origin and pixel size of Th tif file
-print 'LP_GRS_Th_Global_2ppd.tif'
+print('LP_GRS_Th_Global_2ppd.tif')
 transform = thTifFile.GetGeoTransform()   # overwrite our old variable transform
 if transform:
     print("Origin of Th tif file = ({}, {})".format(transform[0], transform[3]))
@@ -158,6 +172,6 @@ if transform:
 # calculate max, min, and average for Th tif file
 indexOfMax = getMaxIndex2d(thTifFile)
 indexOfMin = getMinIndex2d(thTifFile)
-print 'Max value is at index [', indexOfMax[0][0], '][', indexOfMax[1][0], '] = ', thNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]
-print 'Min value is at index [', indexOfMin[0][0], '][', indexOfMin[1][0], '] = ', thNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]
-print 'Mean value is', getAverage(thTifFile)
+print("Max value is at index [{}][{}] = {}".format(indexOfMax[0][0], indexOfMax[1][0], thNumpyArray[indexOfMax[0][0]][indexOfMax[1][0]]))
+print('Min value is at index [{}][{}] = {}'.format(indexOfMin[0][0], indexOfMin[1][0], thNumpyArray[indexOfMin[0][0]][indexOfMin[1][0]]))
+print('Mean value is {}'.format(getAverage(thTifFile)))
