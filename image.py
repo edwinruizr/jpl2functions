0f742f532b56174d0c9c0addb472e613c6b2b743
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-11-01 19:12:41 -0700

    made function receive gdal file to get projection from it and use it as the in projection also made it return tuple rather than print it

--- image.py
+++ image.py
@@ -9,6 +9,7 @@ import numpy
 import sys
 import numpy.ma as ma
 import logging
+
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
 logging.debug('Start of program')
 # FUNCTION DEFINITIONS - START
@@ -63,13 +64,18 @@ def getXCoordinate(indexX, pixel_x, upper_left_lat):
 def getYCoordinate(indexY, pixel_y, upper_left_lon):
     return upper_left_lon-indexY*pixel_y
 
-def convertToLatLong(coordX,coordY):
-#    inProj = Proj(init='epsg:4326')
-    inProj = Proj('+proj=eqc +lat_ts=20 +lat_0=0 +lon_0=180 +x_0=0 +y_0=0 +a=1737400 +b=1737400 +units=m +no_defs ')
+
+def convertToLatLong(coordX,coordY, gdalFile):
+    inSRS_wkt = gdalFile.GetProjection()  # gives SRS in WKT
+    logging.debug(inSRS_wkt)
+    inSRS_converter = osr.SpatialReference()  # makes an empty spatial ref object
+    inSRS_converter.ImportFromWkt(inSRS_wkt)  # populates the spatial ref object with our WKT SRS
+    inSRS_forPyProj = inSRS_converter.ExportToProj4()  # Exports an SRS ref as a Proj4 string usable by PyProj
+    inProj = Proj(inSRS_forPyProj)
+    logging.debug(inSRS_forPyProj)
     outProj = Proj(init='epsg:4326')
-    x1,y1 = coordX,coordY
-    x2,y2 = transform(inProj,outProj,x1,y1)
-    print (x2,y2)
+    return transform(inProj,outProj,coordX,coordY)
+
 
 #
 #
@@ -149,8 +155,10 @@ print (getYCoordinate(0, geotransform[5], geotransform[3]))
 print (getXCoordinate(5286, geotransform[1], geotransform[0]))
 print (getYCoordinate(14695, geotransform[5], geotransform[3]))
 
-print("convert lat to long")
-convertToLatLong(geotransform[0], geotransform[3])
+convertToLatLong(geotransform[0], geotransform[3],slopeTifFile)
+
+x2,y2 = convertToLatLong(-4838396, 622172, slopeTifFile)
+print("({},{})".format(x2,y2))
 
 # get origin and pixel size of fe tif file
 #print('LP_GRS_Fe_Global_2ppd.tif')
